50a15302f2434182c64925bf32cde72b7b4478cc
wenrongchen <43869196+wenronchen@users.noreply.github.com>
2023-03-27 15:06:19 -0500

    Add files via upload

--- find_confident_ptm.py
+++ find_confident_ptm.py
@@ -32,10 +32,6 @@ def get_ptm_seq(tmp):
     tmp=tmp[flag+1:]
     flag=tmp.find(".")
     tmp=tmp[:flag]
-    
-    # while(tmp.find("(C)")!=-1):
-    #     flag=tmp.find("(C)")
-    #     tmp=tmp[:flag]+tmp[flag+1]+tmp[flag+3:]
         
     
     flag1=tmp.find("(")
@@ -55,16 +51,8 @@ def compare_mass(m1, m2, t):##determine if the m1-m2, m1-m2+1.00235, m1-m2-1.002
 
 
 df=pd.read_csv(sys.argv[1],sep='\t')
-#ptm_value=float(sys.argv[2])
 
-#potential_aa=["M","D","K","N","P","Y","R","C"] ##oxidation 15.9949
-#potential_aa=["S","T","Y"] ##Phosphorylation 79.9663
-#potential_aa=["C","I","L","K","R","H","D","E","N","Q","S","T"] # Methylation 14.0157
-#potential_aa=["K","C","S","T"] ##Acetylation 42.0106
-#potential_aa=["K","N","R"] ##Dimethylation, 28.0313
-#potential_aa=["N","Q"] ##Deamination, 0.9840
-#potential_aa=["D","E"] ##Cation:Na, 21.9819; Cation:K, 37.95582
-
+##PTMs with high frequency from bottom-up MS
 potential_aa_dict={21.981943:["D","E"],37.955882:["D","E"],52.911464:["D","E"],53.919289:["D","E"],\
                    43.005814:["K","R","C","M","S","T","Y"],79.966331:["S","T","Y"],183.035399:["H","K","S","Y"],\
                        14.01565:["C","I","L","K","R","H","D","E","N","Q","S","T"],28.0313:["K","N","R","P"],\
@@ -88,20 +76,18 @@ for i in range(df.shape[0]):
                 if(aa in ptm_seq):
                     remained.append(i)
                     break
-            
-        # if((ptm_last-ptm_first)<3):
-        #     remained.append(i)
 
-print(cnt)
+
+#print(cnt)
 remained=list(set(remained))
-print(len(remained))
+#print(len(remained))
 
 output_df=df.loc[remained]
 output_df.to_csv(sys.argv[2],sep='\t',index=None)
 
-class_four_df=df.drop(remained)
-print(class_four_df.shape[0])
-class_four_df.to_csv(sys.argv[3],sep='\t',index=None)
+#class_four_df=df.drop(remained)
+#print(class_four_df.shape[0])
+#class_four_df.to_csv(sys.argv[3],sep='\t',index=None)
 
 
     
